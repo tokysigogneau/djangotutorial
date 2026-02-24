f245c189c063d1c9b98982cfdf119dbcfc0362e7
tokytoky <toky.sigogneau@fms-ea.com>
2026-02-24 08:53:43 +0100

    2.2.1.4.1 finished

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,6 +1,23 @@
-from django.contrib import admin
+import datetime
+
+from django.contrib import admin  # type: ignore
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text"]
+    list_filter = [datetime.datetime(2026,2,24)]
+
+    @admin.display(empty_value="???")
+    def view_question(self, obj):
+        return  obj.question_text
+
+class ChoicesAdmin(admin.ModelAdmin):
+    list_display = ["choice_text"]
+
+    @admin.display(empty_value="???")
+    def view_choices(self, obj):
+        return obj.choice_text
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice, ChoicesAdmin)
