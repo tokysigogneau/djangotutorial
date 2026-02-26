026d33969b033e9a5a6f8fd642e1d57ffef17f5b
tokytoky <toky.sigogneau@fms-ea.com>
2026-02-26 15:35:20 +0100

    4.1 Tuto part 5 : updated admin.py : changed class QuestionAdmin() and added class ChoiceInLine()

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -4,11 +4,16 @@ from django.contrib import admin  # type: ignore
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 class ChoicesAdmin(admin.ModelAdmin):
     list_display = ["choice_text"]
@@ -16,5 +21,7 @@ class ChoicesAdmin(admin.ModelAdmin):
     ordering = ["votes"]
     search_fields = ["foreign_key__related_fieldname"]
 
+
+
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice, ChoicesAdmin)
+
