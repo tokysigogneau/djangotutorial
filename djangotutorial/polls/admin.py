0666a472ec9ae3d9575d75c209e68af39ae20769
tokytoky <toky.sigogneau@fms-ea.com>
2026-02-26 15:30:01 +0100

    4.1 Tuto part 5 : updated admin.py : changed class QuestionAdmin()

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -5,7 +5,10 @@ from django.contrib import admin  # type: ignore
 from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 class ChoicesAdmin(admin.ModelAdmin):
     list_display = ["choice_text"]
@@ -13,6 +16,5 @@ class ChoicesAdmin(admin.ModelAdmin):
     ordering = ["votes"]
     search_fields = ["foreign_key__related_fieldname"]
 
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice, ChoicesAdmin)
